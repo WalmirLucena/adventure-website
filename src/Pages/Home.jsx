7dac8e63d7262900dffbccf84b68af07f7e1587b
WalmirLucena <walmirlucena3@gmail.com>
2021-12-13 14:15:04 -0400

    Desenvolvendo o Header

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+function Home(){
+  return (
+    <div>
+      <h1>oi</h1>
+      <Header/>
+    </div>
+    )
+}
+ export default Home;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return (
+        <header class="header">
+        <a href="#" class="logo"><i class="fas fa-hiking"></i> Explore Amazonia</a>
+        </header>
+    )
+}
+ export default Header;
